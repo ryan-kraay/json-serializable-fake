a6399a903137c7cf6b84b21c6309332378b9ec78
Ryan Kraay <6840071+ryan-kraay@users.noreply.github.com>
2023-06-25 21:15:18 +0200

    Replace testing with Spectator

--- spec/serializer_fake_spec.cr
+++ spec/serializer_fake_spec.cr
@@ -1,9 +1,9 @@
 require "./spec_helper"
 
-describe SerializerFake do
+Spectator.describe SerializerFake do
   # TODO: Write tests
 
   it "works" do
-    false.should eq(false)
+    expect(true).to be_true
   end
 end
